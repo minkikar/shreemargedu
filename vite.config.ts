8a7bc52451cfdc42c80b7baa0b9df1763227d86a
minkikar <minkikaishrao@gmail.com>
2026-03-17 00:14:48 +0530

    small1

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  // ADD THIS LINE BELOW
-  base: '/shreemargedu/', 
+  // This automatically sets the base to '/' for Vercel/Local 
+  // and '/shreemargedu/' for GitHub Pages
+  base: process.env.VERCEL || process.env.NODE_ENV === 'development' ? '/' : '/shreemargedu/',
 
   plugins: [
     react(),
